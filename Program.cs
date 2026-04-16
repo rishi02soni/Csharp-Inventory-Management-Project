66329472d1fa6190c570cd1ea5dd8448eec581eb
Rishi Soni <167216843+rishi02soni@users.noreply.github.com>
2026-04-16 22:57:14 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using InventoryManagement.Services;
+
+var service = new InventoryService();
+
+while (true)
+{
+    Console.WriteLine("\n=== Inventory Management ===");
+    Console.WriteLine("1. Add Product");
+    Console.WriteLine("2. View Products");
+    Console.WriteLine("3. Search Product");
+    Console.WriteLine("4. Delete Product");
+    Console.WriteLine("5. Exit");
+    Console.Write("Choose: ");
+
+    var choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        case "1":
+            service.AddProduct();
+            break;
+        case "2":
+            service.ViewProducts();
+            break;
+        case "3":
+            service.SearchProduct();
+            break;
+        case "4":
+            service.DeleteProduct();
+            break;
+        case "5":
+            return;
+        default:
+            Console.WriteLine("Invalid option");
+            break;
+    }
+}
